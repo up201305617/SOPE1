6a9865090ff3f2f2a35d048a471b3d0f4b098d0f
up201305617 <up201305617@fe.up.pt>
2016-04-06 13:56:27 +0100

    já ordena e remove ficheiro temporário.

--- lstdir.c
+++ lstdir.c
@@ -108,7 +108,6 @@ int main(int argc, char* argv[])
 			}
 	    }
 	}
-
 	return 0;
 }
 /*
--- rmdup.c
+++ rmdup.c
@@ -14,13 +14,16 @@
 int main(int argc, char* argv[])
 {
   DIR *dir;
-  int fd1;
+  int fd1; //ficheiro temporário com a lista de files desordenada
+  int fd2; //ficheiro final com a lista de files ordenada
   struct dirent *direntp;
   struct stat stat_buf_1;
   int child_counter=0;
   pid_t pids[128];
-
-  char *filename = "files_aux.txt";
+  int rem_status;
+  char *filename = "/tmp/files_aux.txt";
+  char *filename1 = "/tmp/files.txt";
+  int saved_stdout = dup(STDOUT_FILENO);
 
   if (argc != 2)
   {
@@ -39,6 +42,13 @@ int main(int argc, char* argv[])
 	perror(filename);
 	exit(3);
   }
+
+  if((fd2=open(filename1,O_WRONLY|O_CREAT|O_APPEND|O_SYNC, 0600)) == -1)
+  {
+	  perror(filename1);
+	  exit(4);
+  }
+
   char str[15];
   sprintf(str, "%d", fd1);
 
@@ -51,7 +61,6 @@ int main(int argc, char* argv[])
 
   char path[PATH_MAX + 1];
   snprintf(path, PATH_MAX + 1, "%s/lstdir", cwd);
-  printf("%s\n",path);
 
   if (chdir(argv[1]) != 0)
   {
@@ -70,7 +79,13 @@ int main(int argc, char* argv[])
      if(S_ISDIR(stat_buf_1.st_mode) && strcmp(direntp->d_name,".")!=0 && strcmp(direntp->d_name,"..")!=0)
      {
     	pid_t pid=fork();
-        if(pid==0)
+
+    	if (pid == -1)
+    	{
+    		 printf ("Error on fork().\n");
+    	}
+
+    	if(pid==0)
         {
         	chdir(direntp->d_name);
         	execl(path,"lstdir",str,NULL);
@@ -88,6 +103,39 @@ int main(int argc, char* argv[])
   {
 	  waitpid(pids[k],NULL,0);
   }
+  dup2(fd2, STDOUT_FILENO);
+
+  pid_t pid1 = fork();
+  int stat;
+
+  if (pid1 == -1)
+  {
+       printf ("Error on fork().\n");
+  }
+  if(pid1==0)
+  {
+	  execlp ("sort", "sort", filename, NULL);
+	  perror("execlp");
+  }
+  if(pid1>0)
+  {
+	  pid_t pid0 = wait(&stat);
+	  printf ("My son (%d) just finished.\n", pid0);
+  }
+
+  close(fd1);
+
+  dup2(saved_stdout, STDOUT_FILENO);
+
+  close(fd2);
+  closedir(dir);
+
+  rem_status=remove(filename);
+
+  if (rem_status == 0)
+      printf ("\nFile %s removed.\n",filename);
+  else
+      printf ("\nFile %s not removed.\n",filename);
 
   return 0;
 }
